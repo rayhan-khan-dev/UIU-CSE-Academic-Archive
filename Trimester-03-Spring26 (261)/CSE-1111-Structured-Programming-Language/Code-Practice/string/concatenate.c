09012d21e40321f18578bf6cf74fa390a5f76130
Rayhan Khan <rayhankhanofficial24@gmail.com>
2026-05-11 21:55:37 +0600

    theory code

---
+++ Trimester-03-Spring26 (261)/CSE-1111-Structured-Programming-Language/Code-Practice/string/concatenate.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str1[100] = "Hello ";
+    char str2[] = "World";
+
+    strcat(str1, str2);
+
+    printf("%s", str1);
+
+    return 0;
+}
